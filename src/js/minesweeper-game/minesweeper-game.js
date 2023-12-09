5faedefcfa9f3d50f3f497d9f81b9aa8dfbdb484
Adri-devnull <adri.devnull@gmail.com>
2023-12-09 14:13:40 +0100

    reparing flags clickeds

--- src/js/minesweeper-game/minesweeper-game.js
+++ src/js/minesweeper-game/minesweeper-game.js
@@ -7,23 +7,27 @@ const flagsButtonElement = document.getElementById('flag-btn');
 
 
 // VARIABLES
-const minesCount = 6;
+let minesCount = 20;
 const minesLocation = [];
 const rows = 8;
 const columns = 8;
 const board = [];
 let flagEnabled = false;
-// let gameOver = false;
-
+let gameOver = false;
+let tilesCompleted = 0;
 
 // FUNCION PARA CREAR LA LOCALIZACION DE LAS MINAS
 const setMines = () => {
-    minesLocation.push('2-2');
-    minesLocation.push('0-2');
-    minesLocation.push('3-1');
-    minesLocation.push('5-5');
-    minesLocation.push('3-2');
-    minesLocation.push('4-6');
+    while (minesCount > 0) {
+        const rRandom = Math.floor(Math.random() * rows);
+        const cRandom = Math.floor(Math.random() * columns);
+        const mineLocation = rRandom.toString() + '-' + cRandom.toString();
+        if (!minesLocation.includes(mineLocation)) {
+            minesLocation.push(mineLocation);
+            minesCount -= 1
+        }
+    }
+
 }
 
 // FUNCION PARA CREAR EL TABLERO DEL JUEGO
@@ -43,7 +47,6 @@ const startGame = () => {
         }
         board.push(row)
     }
-    console.log(board);
 }
 
 // LLAMAMOS A LA FUNCION PARA CREAR EL TABLERO
@@ -65,29 +68,103 @@ const revealMines = () => {
     for (let r = 0; r < rows; r++) {
         for (let c = 0; c < columns; c++) {
             const tile = board[c][r];
-            if (minesLocation.includes(tile.dataset.id)){
-                tile.textContent = '💣';
+            if (minesLocation.includes(tile.dataset.id)) {
                 tile.classList.add('bomb-clicked');
+                tile.textContent = '💣';
             }
         }
     }
 }
 
-// FUNCION PARA PONER BANDERAS EN LAS CELDAS
+// FUNCION PARA CUANDO SE CLICA UNA DE LAS CELDAS
 const clickTile = (tile) => {
+    if (gameOver || tile.classList.contains('tile-clicked')) {
+        return;
+    }
     if (flagEnabled) {
         if (tile.textContent === '') {
             tile.textContent = '🚩';
-        } else if (tile.textContent === '🚩') {
+        }if (tile.textContent === '🚩') {
             tile.textContent = '';
         }
     }
 
     if (minesLocation.includes(tile.dataset.id)) {
-        console.log('BOOM');
-        gameOver = true; // aqui terminaria el juego porque se ha clicado en una bomba
+        tile.classList.add('bomb-clicked');
+        tile.textContent = '💣';
+        gameOver = true;
         revealMines();
+        return; // Sale de 
     }
+
+    const coords = tile.dataset.id.split('-');
+    const r = parseInt(coords[0]);
+    const c = parseInt(coords[1]);
+    checkMine(r, c);
+}
+
+// COMPROBAR LAS MINAS DE ALREDEDOR QUE TENGLA CADA CELDA
+const checkMine = (r, c) => {
+    if (r < 0 || r > rows || c < 0 || c > columns) {
+        return;
+    }
+
+    if (board[r][c].classList.contains('tile-clicked')) {
+        return;
+    }
+
+    tilesCompleted += 1
+
+    board[r][c].classList.add('tile-clicked');
+    let minesFound = 0;
+
+    // top 3
+    minesFound += checkTile(r - 1, c - 1); // top left
+    minesFound += checkTile(r - 1, c); // top
+    minesFound += checkTile(r - 1, c + 1); // top right
+
+    // middle 3
+    minesFound += checkTile(r, c - 1); // middle left
+    minesFound += checkTile(r, c + 1); // middle right
+
+    // bottom 3
+    minesFound += checkTile(r + 1, c - 1); // bottom left
+    minesFound += checkTile(r + 1, c); // bottom
+    minesFound += checkTile(r + 1, c + 1); // bottom right
+
+    if (minesFound > 0) {
+        board[r][c].textContent = minesFound;
+        board[r][c].classList.add("x" + minesFound.toString());
+    } else {
+        minesFound += checkMine(r - 1, c - 1); // top left
+        minesFound += checkMine(r - 1, c); // top
+        minesFound += checkMine(r - 1, c + 1); // top right
+
+        // middle 3
+        minesFound += checkMine(r, c - 1); // middle left
+        minesFound += checkMine(r, c + 1); // middle right
+
+        // bottom 3
+        minesFound += checkMine(r + 1, c - 1); // bottom left
+        minesFound += checkMine(r + 1, c); // bottom
+        minesFound += checkMine(r + 1, c + 1); // bottom right
+    }
+
+    if (tilesCompleted === columns * rows - minesCount) {
+        gameOver = true;
+        alert('you win') // AQUI VA EL MENSAJE O EL BANNER DE CUANDO EL USUARIO LIMPIA TODAS LAS MINAS
+    }
+}
+
+/// CHECKTILE FUNCTION
+const checkTile = (r, c) => {
+    if (r < 0 || r > rows || c < 0 || c > columns) {
+        return 0;
+    }
+    if (minesLocation.includes(r.toString() + '-' + c.toString())) {
+        return 1;
+    }
+    return 0;
 }
 
 // EVENTO DE ESCUCHA PARA EL BOTON DE PONER BANDERAS
